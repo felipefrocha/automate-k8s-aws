d74feb3e43e879ef4b5d552274065692bc96d777
Felipe F. Rocha <felipefonsecarocha@gmail.com>
2020-05-26 01:43:55 -0300

    CMD to kubectl

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,8 +83,8 @@ resource "aws_security_group" "sg_public_html" {
   ingress {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    from_port = 80
-    to_port   = 80
+    from_port = 30000
+    to_port   = 32767
   }
 
   egress {
